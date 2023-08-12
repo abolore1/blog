183d5d2e0d56a360948a700eda50741bd6b187ca
abolore1 <qudus_kudos@yahoo.com>
2023-08-12 09:52:37 +0100

    adding project directories

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Navbar from './Navbar';
+import Home from './Home'
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+
+
+function App(){
+  return (
+   <Router>
+       <div className='App'>
+      <div className="content">
+        <Navbar/>
+       <Switch>
+        <Route exact path='/'>
+         <Home/>
+        </Route>
+        <Route  path='/create'>
+         <Create/>
+        </Route>
+        <Route  path='/blogs/:id'>
+         <BlogDetails/>
+        </Route>
+       </Switch>
+      </div>
+    </div>
+   </Router>
+  )
+}
+ 
+export default App ;
